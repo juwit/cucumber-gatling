eb77142fbdd033bf0cdb05fd8cfb586d274106c6
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-07-29 07:56:56 +0200

    ✅ : add integration test

---
+++ src/test/java/io/codeka/GatlingMainTest.java
@@ -0,0 +1,19 @@
+package io.codeka;
+
+import io.gatling.app.Gatling$;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GatlingIntegrationTest {
+
+    @Test
+    void testGatlingRun() {
+        int returnCode = Gatling$.MODULE$.fromArgs(new String[]{
+                "--simulation=io.codeka.CucumberSimulation",
+                "--no-reports",
+                "--results-folder=target/"
+        });
+        assertEquals(0, returnCode);
+    }
+}
